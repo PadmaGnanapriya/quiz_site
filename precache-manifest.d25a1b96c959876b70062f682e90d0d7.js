662b4a404aaa63a0997a36ee127ade0a2190a010
PadmaGnanapriya <padmaisuru@gmail.com>
2020-10-05 17:01:18 +0530

    Updates

---
+++ precache-manifest.d25a1b96c959876b70062f682e90d0d7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "276e5a0e1977e7562e17c23376048f56",
+    "url": "/quiz_site/index.html"
+  },
+  {
+    "revision": "58ddfd4e192dd163863f",
+    "url": "/quiz_site/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "52cd612f05f364211189",
+    "url": "/quiz_site/static/js/2.b36d7125.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/quiz_site/static/js/2.b36d7125.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "58ddfd4e192dd163863f",
+    "url": "/quiz_site/static/js/main.1bbfeb8d.chunk.js"
+  },
+  {
+    "revision": "ae67d1f647a8d9221b2a",
+    "url": "/quiz_site/static/js/runtime-main.da718a44.js"
+  }
+]);
