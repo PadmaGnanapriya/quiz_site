8ac9d415c52e3230dc09c2524597a97708be9c60
PadmaGnanapriya <padmaisuru@gmail.com>
2020-10-05 10:28:13 +0530

    Make score show after finish

--- src/App.js
+++ src/App.js
@@ -4,21 +4,58 @@ import Questionaire from "./Questionaire";
 const API_URL=
     'https://opentdb.com/api.php?amount=10&category=18&type=multiple';
 function App() {
-  const[questions, setQuesions]=useState([]);
+  const [questions, setQuesions]=useState([]);
+  const [currentIndex, setCurrentIndex]= useState(0);
+  const [score, setScore]=useState(0);
+  const [gameEnded, setGameEnded]=useState(false);
+
   useEffect(() => {
     fetch(API_URL)
         .then((res)=>res.json())
         .then((data) => {
           setQuesions(data.results);
+          setCurrentIndex(0);
         });
   },[]);
 
   const handelAnswer =(answer)=>{
+    //Check the answer
+    const newIndex =currentIndex+1;
+    setCurrentIndex(newIndex);
+    setCurrentIndex(currentIndex+1);
+    if(answer===questions[currentIndex].correct_answer){
+        setScore(score+1);
+    }
+    if(newIndex>=questions.length){
+        setGameEnded(true);
+    }
+
+
+    //Show another answer
+
+    //change score if correct
+
 
   };
+  return gameEnded ?  (
+      <div> Your Score is {score}</div>
+  ):(
+      questions.length>0?(
+          <div className='container'>
+              <Questionaire
+                  data={questions[currentIndex]}
+                  handleAnswer={handelAnswer}
+              />
+          </div>
+      ):(
+          <h2>Loading</h2>
+      )
+  );
 
   return questions.length>0?(<div className="container" >
-    <Questionaire data={questions[0]}handleAnswer={handelAnswer} />
+
+      <Questionaire data={questions[currentIndex]}handleAnswer={handelAnswer} />
+
   </div>):null;
 }
 
--- src/Questionaire.js
+++ src/Questionaire.js
@@ -1,8 +1,5 @@
 import React from "react";
 
-const Button =({answer, className})=> (
-    <button className={`bg-while p-4 text-purple round shadow ${className}`}>{answer}</button>
-);
 
 const Questionaire =({handleAnswer, data:{question, correct_answer, incorrect_answers},
 }) => {
@@ -14,10 +11,10 @@ const Questionaire =({handleAnswer, data:{question, correct_answer, incorrect_an
         <hr className="p-6"></hr>
         <div className="p-2">{question} ****</div>
         <div className="grid cols-2 gap-2 mt-4 p-2">
-            <Button className={correct_answer===shuffledAnswer[0]?'bg-purple-300':''} onClick={()=>handleAnswer(shuffledAnswer[0])} answer={shuffledAnswer[0]} />
-            <Button className={correct_answer===shuffledAnswer[1]?'bg-purple-300':''} onClick={()=>handleAnswer(shuffledAnswer[1])} answer={shuffledAnswer[1]} />
-            <Button className={correct_answer===shuffledAnswer[2]?'bg-purple-300':''} onClick={()=>handleAnswer(shuffledAnswer[2])} answer={shuffledAnswer[2]} />
-            <Button className={correct_answer===shuffledAnswer[3]?'bg-purple-300':''} onClick={()=>handleAnswer(shuffledAnswer[3])} answer={shuffledAnswer[3]} />
+            {shuffledAnswer.map((answer)=>(
+                <button className={`${correct_answer===answer? 'bg-purple-300': 'bg-white'} p-4 text-purple-800 round shadow`}
+                    onClick={()=>handleAnswer(answer)}>{answer}</button>
+            ))}
         </div>
     </div>
 )};
